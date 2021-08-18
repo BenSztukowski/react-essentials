e180382d66dd3ce129c3e9b24eed111922257e13
Ben <bensztukowski@gmail.com>
2021-08-18 11:33:23 -0500

    interesting hook use effects

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
-
 
 function App(props) {
   const [emotion, setEmotion] = useState("happy");
-  console.log(emotion);
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]);
+
+  useEffect(()=> {
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary])
+
   return (
     <>
-     <h1>Current emotion is {emotion}.</h1>
+     <h1>Current emotion is {emotion} and {secondary}.</h1>
      <button onClick={() => setEmotion("happy")}>
-      Happy
+      Make Happy
+     </button>
+     <button onClick={()=> setSecondary("crabby")}>
+       Make crabby
      </button>
      <button onClick={() => setEmotion("frustrated")}>
       Frustrate
