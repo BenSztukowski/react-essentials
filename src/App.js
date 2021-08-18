e32c4106adaefc7cf966972791dbc66635c288b9
Ben <bensztukowski@gmail.com>
2021-08-18 11:22:15 -0500

    use state hooks

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
-
+import React, { useState } from "react";
 import './App.css';
 
-function Header(props) {
-  console.log(props);
+
+function App(props) {
+  const [emotion, setEmotion] = useState("happy");
+  console.log(emotion);
   return (
-    <header>
-      <h1>{props.name} lord</h1>
-    </header>
-  );
-}
-
-function Main(props){
-  return (
-    <section>
-      <p>I'm {props.adjective}.</p>
-      <ul>
-        {props.dishes.map((dish) => <li key={dish.id}>{dish.title}</li>)}
-      </ul>
-    </section>
-  );
-}
-
-function Footer (props) {
-  return(
-  <p>I'm really cool on this year: {props.year}</p>
-  );
-}
-
-const dishes = [
-  "macoroni",
-  "donkey meat",
-  "gross stuff"
-];
-
-const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish}));
-
-dishes.map((dish) => console.log(dish));
-
-function App() {
-  return (
-    <div className="App">
-      <Header  name="Ben"/>
-      <Main adjective="whatever" dishes={dishObjects}/>
-      <Footer year={new Date().getFullYear()}/>
-    </div>
+    <>
+     <h1>Current emotion is {emotion}.</h1>
+     <button onClick={() => setEmotion("happy")}>
+      Happy
+     </button>
+     <button onClick={() => setEmotion("frustrated")}>
+      Frustrate
+     </button>
+     <button onClick= {() => setEmotion("enthusiastic")}>
+       Enthuse
+     </button>
+    </>
   );
 }
 
